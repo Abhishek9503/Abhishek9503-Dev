d2fe4f9648596bf734e2b8cdc191635d44a86a71
Abhishek <yadavabhishek9503@gmail.com>
2023-10-09 17:14:55 +0530

    backtotop added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import { BrowserRouter } from "react-router-dom";
 import {About,Contact, Experience, Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from './components'
 import Tweets from "./components/Tweets";
+import { useState, useEffect } from "react";
 import CV from "./components/CV";
 import Footer from "./components/Footer";
+import { BsArrowUp } from 'react-icons/bs'
 // import PreLoader from "./components/PreLoader";
 // import { RingLoader } from "react-spinners";
 const  App=()=> {
+
+ 
+  const [showBackToTop, setShowBackToTop] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > -0.1) {
+        setShowBackToTop(true);
+      } else {
+        setShowBackToTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
 
     <>
@@ -40,6 +66,20 @@ const  App=()=> {
 
  </div>
  </BrowserRouter>
+
+
+ {showBackToTop && (
+        <button
+          className="fixed bottom-4 right-4 p-2 shadow cursor-pointer backToTop"
+          onClick={handleBackToTop}
+        >
+          <BsArrowUp />
+          
+        </button>
+      )}
+
+
+
     </>
 
  
