947e6e0380b28a5f5a4472e815e948125ac99756
Abhishek <abhishek.pub9503@gmail.com>
2024-06-09 23:56:24 +0530

    Added workflow

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Analytics } from "@vercel/analytics/react"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
- 
- 
     <App />
-    <Analytics/>
-  </React.StrictMode>,
-)
+    <Analytics />
+  </React.StrictMode>
+);
